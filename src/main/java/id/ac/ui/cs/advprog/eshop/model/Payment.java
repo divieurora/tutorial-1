5845ee0b86cb5e0568807d02ffb829343c551918
divieurora <divieuroraa@gmail.com>
2024-03-06 09:07:30 +0700

    [GREEN] Implement Payment model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -13,8 +13,40 @@ public class Payment {
     Order order;
 
     public Payment(String id,  Order order, String method, Map<String, String>paymentData){
+        this(order, method, paymentData);
+        this.id = id;
     }
     public Payment(Order order, String method, Map<String, String>paymentData){
+        if (method == null || method.trim().isEmpty()) {
+            throw new IllegalArgumentException("Method cannot be empty");
+        }
+        this.id = UUID.randomUUID().toString();
+        this.method = method;
+        this.order = order;
+        setPaymentData(paymentData);
     }
 
+    private void setPaymentData(Map<String, String>paymentData){
+        if (method.equals("VOUCHER")){
+            int numbers = 0;
+            for (int i=0; i<paymentData.get("voucherCode").length(); i++){
+                if (Character.isDigit(paymentData.get("voucherCode").charAt(i))){
+                    numbers+=1;
+                }
+            }
+            if (paymentData.get("voucherCode").length()!=16 ||
+                    !paymentData.get("voucherCode").startsWith("ESHOP") ||
+                    numbers!=8){
+                throw new IllegalArgumentException();
+            }
+        } else if (method.equals("COD")){
+            if (paymentData.get("address").isBlank() ||
+                    paymentData.get("deliveryFee").isBlank()){
+                throw new IllegalArgumentException();
+            }
+        } else {
+            throw new IllegalArgumentException();
+        }
+        this.paymentData = paymentData;
+    }
 }
